2ab4bb37043bad31823d4bae788aaf0c283b6c99
hkumar1403 <kumarhemant1403@gmail.com>
2026-07-31 17:03:09 +0530

    Fix dashboard layout grid and wake-up calls card positioning

--- components/dashboard/MainDashboardView.tsx
+++ components/dashboard/MainDashboardView.tsx
@@ -73,7 +73,6 @@ export function MainDashboardView() {
       navItems={navItems}
       user={currentUser}
     >
-      <WakeUpCallsAlert calls={wakeUpCalls} />
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6 mb-8">
         {summaryStats.map((stat, index) => (
           <StatCard key={index} stat={stat} />
@@ -81,11 +80,14 @@ export function MainDashboardView() {
       </div>
 
       <div className="grid grid-cols-1 lg:grid-cols-3 gap-8 mb-8">
-        <div className="lg:col-span-2">
+        <div>
           <GuestsChart data={guestData} />
         </div>
         <div>
           <RevenueChart data={revenueData} />
+        </div>
+        <div>
+          <WakeUpCallsAlert calls={wakeUpCalls} />
         </div>
       </div>
 
